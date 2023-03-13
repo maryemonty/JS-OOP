161541aa7ca46a85ea0bf510b75a160ccda318ab
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-03-13 17:34:36 +0100

    aggio

--- script.js
+++ script.js
@@ -5,23 +5,16 @@ class User{
         this.age = age;
         this.location = location
     }
+
+    static age(x,y){
+        return x.age > y.age ? `${x.firstName} ${x.lastName} è più vecchio di ${y.firstName} ${y.lastName}` : `${y.firstName} ${y.lastName} è più vecchio di ${x.firstName} ${x.lastName}`
+    }
 }
 
 const x = new User('Carlo','Carletti',1,'Bali')
 const y = new User('Carla','Carletta',98,'Francia')
 
-console.log(x.age);
-console.log(y.age);
-
-function comp(){
-    if (x.age > y.age){
-        return `${x.firstName} ${x.lastName} è più vecchio di ${y.firstName} ${y.lastName}`
-    }else{
-        return `${y.firstName} ${y.lastName} è più vecchio di ${x.firstName} ${x.lastName}`
-    }
-}
-
-console.log(comp());
+console.log(User.age(x,y));
 
 class Pet{
     constructor(petName, ownerName, species, breed){
